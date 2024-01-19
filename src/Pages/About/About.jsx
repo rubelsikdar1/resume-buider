b2ee772e07008c6de022bf357c1b19d62b49a9b0
rubelsikdar1 <rubelsidar799@hotmail.com>
2024-01-19 23:06:25 +0600

    [Done] some update

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -8,7 +8,7 @@ const testimonials = [
         content:
             'A resume builder website is a web-based tool that allows users to create and customize a professional resume to their desired specifications. These websites typically provide templates for creating a resume.',
         avatar:
-            'https://ibb.co/F3PC595',
+            URL("./my-pic.png"),
     }
 ];
 
